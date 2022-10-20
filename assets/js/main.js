a072e60df24f62ba81644262b8e9c27dd9557c02
i6rahim <i6rahim210@gmail.com>
2022-10-21 01:20:01 +0300

    "add some sections"

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,53 @@ btn.forEach(e => {
         }
     })
 })
+
+
+
+function headerNavShow() {
+    const burger = document.querySelector('.header__burger'),
+          nav = document.querySelector('.header__nav');
+
+    burger.addEventListener('click', (el)=> {
+        nav.classList.toggle('active')
+        burger.classList.toggle('active')
+    })
+}
+headerNavShow()
+
+
+function accordionOpen() {
+    const accordionItem = document.querySelectorAll('.accordion__item')
+    accordionItem.forEach(e => {
+        e.addEventListener('click', el => {
+            const self = el.currentTarget;
+            self.classList.toggle('open')
+            console.log(self);
+        })
+    })
+}
+accordionOpen()
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
